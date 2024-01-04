03ff73cc76df581c13caf74973fe7739cc67954d
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-04 13:15:18 -0400

    Update: axios baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "http://192.168.0.42:3000"
-    : "http://localhost:3000";
-
 export const api = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `/api`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
